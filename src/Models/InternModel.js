427f5afdf8592e9abd3ff9fccd247d7c37bc57f0
animeshj2132 <ajangir52@gmail.com>
2022-06-28 17:18:45 +0530

    schema

---
+++ src/Models/InternModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const valid = require('validator')
+
+const internSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: 'name is mandatory',
+        trim: true
+    },
+    email:{
+        type: String,
+        required: 'email is mandatory',
+        unique: true,
+        trim: true
+    },
+    mobile:{
+        type: Number,
+        required: 'mobile  number is mandatory',
+        unique: true,
+        trim: true
+     },
+     collegeID:{
+        type: ObjectId,
+        ref: "college",
+     },
+     isDeleted: {
+        type: Boolean,
+        default: false
+     }
+
+}, {timestamps:true})
+
+module.exports = mongoose.model("intern",internSchema)
